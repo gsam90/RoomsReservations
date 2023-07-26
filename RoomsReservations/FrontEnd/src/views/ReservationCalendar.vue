82f2d0d7eeda4bd4f2b7a311a71ef265f4fdf205
gsam90 <113465605+gsam90@users.noreply.github.com>
2023-07-26 11:20:34 +0300

    dates appearing on calendar

--- RoomsReservations/FrontEnd/src/views/ReservationCalendar.vue
+++ RoomsReservations/FrontEnd/src/views/ReservationCalendar.vue
@@ -10,6 +10,13 @@
                   @eventClick="handleEventClick"
                   /> 
   </div>
+  <div id="reservationModal" class="modal">
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <!-- Reservation details will be displayed here -->
+    <div id="reservationDetails"></div>
+  </div>
+</div>
 </template>
 
 <script setup>
@@ -29,7 +36,15 @@ onMounted(async () =>
   {
     const response = await axios.get('https://localhost:7183/Reservation');
     reservations.value = response.data;
-    console.log(reservations.value);
+
+    // Close the modal when the user clicks on the close button (x)
+    document.querySelector('.close').addEventListener('click', () => {
+    const modal = document.getElementById('reservationModal');
+    modal.style.display = 'none';
+  });
+    
+
+    //console.log(reservations.value);
     
   } 
   catch (error) 
@@ -37,6 +52,7 @@ onMounted(async () =>
     console.error('Error fetching reservations:', error);
   }
 });
+
 
 // Format reservations as events for FullCalendar
 const formattedReservations = computed(() => {
@@ -47,11 +63,11 @@ const formattedReservations = computed(() => {
     const filteredReservations = reservations.value.filter(reservation => {
       const checkInDate = new Date(reservation.checkInDate);
       const checkOutDate = new Date(reservation.checkOutDate);
-      console.log(checkInDate, checkOutDate)
+      //console.log(checkInDate, checkOutDate)
 
       const formattedcheckInDateString = formattedDate(checkInDate);
       const formattedcheckOutDateString = formattedDate(checkOutDate);
-console.log(formattedcheckInDateString, formattedcheckOutDateString);
+//console.log(formattedcheckInDateString, formattedcheckOutDateString);
 
 
     // Check if the selectedDate is within the reservation range
@@ -60,6 +76,15 @@ console.log(formattedcheckInDateString, formattedcheckOutDateString);
     //   selectedDate.value < checkOutDate
       
     // );
+    return {
+      title: reservation.reservationName,
+      start: formattedcheckInDateString,
+      end: formattedcheckOutDateString,
+      allDay: true,
+      editable: true,
+      selectedDate: selectedDate.value,
+      selectable: true
+    }
   });
 
   return filteredReservations.map(reservation => ({
@@ -86,12 +111,29 @@ const handleDateSelect = (arg) => {
   }
   
   const handleEventClick = (arg) => {
+    try {
     const reservation = reservations.value.find(reservation => reservation.checkInDate === arg.event.start);
     if (reservation) {
-      // Do something with the selected reservation, e.g., show a modal with reservation details
-      console.log('Selected Reservation:', reservation);
+       // Show the modal
+      const modal = document.getElementById('reservationModal');
+      modal.style.display = 'block';
+
+       // Display the reservation details in the modal
+      const reservationDetails = document.getElementById('reservationDetails');
+      reservationDetails.innerHTML = `
+        <h3>Reservation Details</h3>
+        <p><strong>Check-in Date:</strong> ${reservation.checkInDate}</p>
+        <p><strong>Other Details:</strong> ${reservation.reservationName}</p>
+        
+        <!-- Add more reservation details here as needed -->`;
+        console.log('Selected Reservation:', reservation);
     }
+  } catch (error) {
+    console.error('Error fetching reservations:', error);
+  }
   };
+
+ 
   
   const calendarOptions = reactive({
     plugins: [dayGridPlugin, interactionPlugin],
@@ -120,4 +162,38 @@ const handleDateSelect = (arg) => {
       --fc-daygrid-event-dot-width: 5px;
       --fc-small-font-size: .85em;
     }
+
+    .modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
 </style>
